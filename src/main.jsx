398748384015cfacc72d1e7deb09540509631316
omr2023 <momoaa792@gmail.com>
2026-01-21 20:58:01 +0200

    fix faq page case-sensitive path

--- src/main.jsx
+++ src/main.jsx
@@ -21,7 +21,7 @@ import ContactsInformation from "./pages/contactsInfomation/ContactsInformation.
 import InvoicesBalances from "./pages/invoicesBalances/InvoicesBalances.jsx";
 import ProfileForm from "./pages/profileForm/ProfileForm.jsx";
 import Calendar from "./pages/calendar/Calendar.jsx";
-import FAQPage from "./pages/FAQPage/faq.jsx";
+import FAQPage from "./pages/fAQPage/faq.jsx";
 import BarChart from "./pages/barChart/BarChart.jsx";
 import PieChart from "./pages/pieChart/PieChart.jsx";
 import LineChart from "./pages/ineChart/LineChart.jsx";
@@ -43,8 +43,8 @@ const router = (setMode) =>
         <Route path="pie" element={<PieChart />} />
         <Route path="line" element={<LineChart />} />
         <Route path="geography" element={<GeographyChart />} />
-      </Route>
-    )
+      </Route>,
+    ),
   );
 
 function Main() {
@@ -69,5 +69,5 @@ function Main() {
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Main />
-  </StrictMode>
+  </StrictMode>,
 );
